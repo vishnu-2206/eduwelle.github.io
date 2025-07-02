aca8da47987b995734697a63ad4f1c378c2027ae
vishnu-2206 <vishnu2206@gmail.com>
2025-07-03 05:20:18 +0530

    .eleventy.js

--- blog/.eleventy.js
+++ blog/.eleventy.js
@@ -1,9 +1,13 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addPassthroughCopy("logo.png");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("logo-reveal.mp4");
+const path = require("path");
 
+module.exports = function(eleventyConfig) {
+  // Passthrough from posts/ to ../docs/blog/
+  eleventyConfig.addPassthroughCopy({ "../style.css": "style.css" });
+  eleventyConfig.addPassthroughCopy({ "../logo.png": "logo.png" });
+  eleventyConfig.addPassthroughCopy({ "../favicon.ico": "favicon.ico" });
+  eleventyConfig.addPassthroughCopy({ "../logo-reveal.mp4": "logo-reveal.mp4" });
+
+  // Date filter
   eleventyConfig.addFilter("readableDate", function(dateObj) {
     return new Date(dateObj).toLocaleDateString("en-US", {
       year: "numeric",
@@ -14,9 +18,9 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: {
-      input: "posts",
-      includes: "../_includes",
-      output: "../docs/blog"
+      input: "posts",             // where your Markdown lives
+      includes: "../_includes",   // path to layout.njk
+      output: "../docs/blog"      // final output
     }
   };
 };
